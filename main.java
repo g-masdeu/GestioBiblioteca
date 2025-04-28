5e90f7263cf96f035c07d1cfbae189aaacd4fd11
CristianBCamp <cristian.perez@insbaixcamp.cat>
2025-04-28 09:55:47 +0200

    Actualizacion main

--- main.java
+++ main.java
@@ -2,9 +2,15 @@
  * Codi per provar la gestió de la biblioteca
  */
 
-public class main {
+ public class main {
     public static void main(String[] args) {
-        System.out.println("Benvingut a la gestió de la biblioteca");
-        System.out.println("-----------------------------------------------");
+    Biblioteca biblioteca = new Biblioteca();
+    Llibre llibre1 = new Llibre("1984", "George Orwell");
+    Llibre llibre2 = new Llibre("El petit príncep", "Antoine de Saint-Exupéry");
+    biblioteca.afegirLlibre(llibre1);
+    biblioteca.afegirLlibre(llibre2);
+    Usuari usuari = new Usuari("Carla");
+    GestorBiblioteca gestor = new GestorBiblioteca();
+    gestor.prestarLlibre(usuari, llibre1);
     }
 }
